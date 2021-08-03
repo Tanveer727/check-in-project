6cfbbf3cfb366089243d211331013d4f26c15dbb
Tanveer727 <79864037+Tanveer727@users.noreply.github.com>
2021-08-03 00:15:33 -0400

    Add files via upload

---
+++ log.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class log : System.Web.UI.Page
+{
+    SqlConnection con;
+    SqlCommand cmd;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+
+
+    
+}
